f78bf9064eb6ff217def94c790ebcb960ba649e2
naomijoyce <naomijoyceb@gmail.com>
2018-08-22 02:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper (string){
 
 function sayHiToGrandma (string){
   if (string == string.toUpperCase()){
-    return "I can't hear you!";
+    return "YES INDEED!";
   }
   if (string == string.toLowerCase()){
-    return "YES INDEED!";
+    return "I can't hear you!";
   }
   if (string == "I love you, Grandma."){
     return "I love you, too."
